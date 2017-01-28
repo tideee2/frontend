f8775bd61e7e89e2745ffd71c340f530bf68f80f
tideee2 <gntnau@gmail.com>
2017-01-28 23:40:28 +0200

    create WDF_PS3

---
+++ WDF_PS3/js/main.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+	$('.modal').modal();
+	//$('.modal-trigger').leanModal();
+
+	$(".button-collapse").sideNav();
+	console.log('ok');
+	$('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'right', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    });
+});
